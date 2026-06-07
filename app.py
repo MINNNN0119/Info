6a117c0c24543d245c5af91c9f62436cc4801218
MINNNN0119 <bruce20060120@gmail.com>
2026-06-07 20:24:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if user_query := st.chat_input("請輸入您的提問..."):
 
                 # 呼叫最新版 Client 的聊天生成
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='Gemini 3.1 Flash Lite',
                     contents=st.session_state.history_contents,
                     config=types.GenerateContentConfig(
                         system_instruction=full_instruction
